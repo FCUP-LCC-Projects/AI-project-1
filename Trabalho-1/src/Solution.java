1b9bdb7cfeb9d2bc6d7a6c32636d8bd8265a8ed4
Fábio Silva <f.m.g.silva@hotmail.com>
2021-03-21 11:27:30 +0000

    Bugs corrigidos e função para detetar todos os conflitos

--- Trabalho-1/src/Solution.java
+++ Trabalho-1/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 //melhor não meter os algoritmos aqui, embora seja chamado de Solution, que isto já tá bue longo
@@ -14,15 +16,18 @@ public class Solution {
 	 */
 	int[] sol;
 	Edge[] edges;
+	Memory mem;
 	int solSize, edgeSize, solMaxSize, edgeMaxSize;
 	
-	Solution(int n){
+	Solution(int n, Memory mem){
 		solMaxSize = n;
 		edgeMaxSize = solMaxSize * solMaxSize;
 		solSize = 0;
 		edgeSize = 0;
 		sol = new int[solMaxSize];
+		Arrays.fill(sol,-1);
 		edges = new Edge[edgeMaxSize]; //n tenho a certeza se é mesmo este o numero maximo de edges dado n pontos
+		this.mem = mem;
 	}
 	
 	//estas funções abaixo são só para facilitar o código, para dar para usá-las como se os arrays fossem listas
@@ -94,7 +99,7 @@ public class Solution {
 		
 		solAdd(start);
 		
-		while(solSize < solMaxSize) {
+		while(solSize <solMaxSize){
 			int next = nearestNeighbour(mem.points[start], mem);
 			solAdd(next);
 			edgeAdd(new Edge(start, next, euclidean(mem, start, next)));
@@ -130,5 +135,146 @@ public class Solution {
 			edgeAdd(new Edge(sol[i], sol[i+1], 
 					euclidean(mem, sol[i], sol[i+1])));
 		}
+
+		int lastEdgeDistance = euclidean(mem, solGetLast(), solGetFirst()); //fica a faltar o último ramo, que é entre o último ponto e o primeiro
+		edgeAdd(new Edge(solGetLast(), solGetFirst(), lastEdgeDistance));
 	}
+
+	// public static Point subPoint(Point a, Point b) {
+	// 	/**
+	// 	 * Subtrai as coordenadas de dois pontos
+	// 	 */
+	// 	return new Point(a.x - b.x, a.y - b.y, -1);
+	// }
+	
+	// public static int crossProduct(Point a, Point b) {
+	// 	/**
+	// 	 * Produto vetorial de dois pontos
+	// 	 */
+	// 	return (a.x * b.y) - (a.y * b.x); 
+	// }
+	
+	// public static int halfStraddle(Point a, Point b, Point c) {
+	// 	/**
+	// 	 * Metade da operação de Straddle de dois pontos
+	// 	 * (saber se um ponto interseta outro)
+	// 	 */
+	// 	return crossProduct(subPoint(c,a),subPoint(b,a));
+	// }
+	
+	// public static int straddleTest(Point a, Point b, Point c, Point d) {
+	// 	/**
+	// 	 * Retorna a posição relativa de um ponto a outro
+	// 	 * (saber se um ponto interseta outro)
+	// 	 */
+	// 	return halfStraddle(c,d,a)*halfStraddle(c,d,b);
+	// }
+	
+	// public static boolean inBox(Point a, Point b, Point c) {
+	// 	/**
+	// 	 * Retorna se um ponto coincide noutro (?)
+	// 	 */
+	// 	return Math.min(a.x,b.x) <= c.x && c.x >= Math.max(a.x,b.x)
+	// 			&& Math.min(a.y, b.y) <= c.y && c.y >= Math.max(a.y,b.y);
+	// }
+	
+	// public static boolean edgeIntersect(Memory mem, Edge i, Edge j) {
+	// 	/**
+	// 	 * Retorna se duas arestas se intersetam
+	// 	 */
+	// 	if(straddleTest(mem.points[i.origin], mem.points[i.dest], mem.points[j.origin], mem.points[j.dest])< 0
+	// 			&& straddleTest(mem.points[j.origin], mem.points[j.dest], mem.points[i.origin], mem.points[i.dest])< 0)
+	// 		return true;
+	// 	else if(halfStraddle(mem.points[i.origin],mem.points[i.dest],mem.points[j.origin]) == 0)
+	// 		if(inBox(mem.points[i.origin], mem.points[i.dest], mem.points[j.origin]))
+	// 			return true;
+	// 	else if(halfStraddle(mem.points[i.origin],mem.points[i.dest],mem.points[j.dest]) == 0)
+	// 		if(inBox(mem.points[i.origin], mem.points[i.dest], mem.points[j.dest]))
+	// 			return true;
+	// 	else if(halfStraddle(mem.points[j.origin],mem.points[j.dest],mem.points[i.origin]) == 0)
+	// 		if(inBox(mem.points[j.origin], mem.points[j.dest], mem.points[i.origin]))
+	// 			return true;
+	// 	else if(halfStraddle(mem.points[j.origin],mem.points[j.dest],mem.points[i.dest]) == 0)
+	// 		if(inBox(mem.points[j.origin], mem.points[j.dest], mem.points[i.dest]))
+	// 			return true;
+	// 	return false;
+			
+	// }
+
+	private int orientation(Point edgeStart, Point edgeEnd, Point p){
+		
+		int value = ((edgeEnd.y - edgeStart.y) * (p.x - edgeEnd.x)) - ((edgeEnd.x- edgeStart.x) * (p.y - edgeEnd.y));
+		if(value ==0){
+			return 0;
+		}
+		if(value >0){
+			return 1;
+		}
+		else{
+			return 2;
+		}
+	}
+	private boolean onSegment(Point edgeStart, Point edgeEnd, Point p){
+        if (edgeEnd.x <= Math.max(edgeStart.x, p.x) && edgeEnd.x >= Math.min(edgeStart.x, p.x) && 
+        
+        edgeEnd.y <= Math.max(edgeStart.y, p.y) && edgeEnd.y >= Math.min(edgeStart.y, p.y)){
+            return true; 
+        } 
+  
+    return false;
+    }
+
+	public boolean edgeIntersect(Edge a, Edge b){
+		int o1, o2,o3,o4;
+		Point start1, end1, start2, end2;
+		start1 = mem.points[a.origin];
+		end1 = mem.points[a.dest];
+		start2 = mem.points[b.origin];
+		end2 = mem.points[b.dest];
+		
+		o1 = orientation(start1, end1, start2);
+		o2 = orientation(start1, end1, end2);
+		o3 = orientation(start2, end2, start1);
+		o4 = orientation(start2, end2, end1);
+		if(o1 != o2 && o3 != o4){
+            return true;
+        }
+
+        if(o1==0  && onSegment(start1, start2, end1)) return true;
+        if(o2==0  && onSegment(start1, end2, end2)) return true;
+        if(o3==0  && onSegment(start2, start1, end2)) return true;
+        if(o4==0  && onSegment(start2, end1, end2)) return true;
+
+        return false;
+
+	}
+	public boolean conflictAlreadyFound(ArrayList<Edge> conflicts, Edge a, Edge b){
+
+		for(int i=0; i<conflicts.size()-1; i+=2){
+			if(conflicts.get(i) == a && conflicts.get(i+1) == b){
+				return true;
+			}
+			if(conflicts.get(i) == b && conflicts.get(i+1) == a){
+				return true;
+			}
+		}
+		return false;
+
+	}
+	public ArrayList<Edge> getAllConflicts(){
+		ArrayList<Edge> conflicts = new ArrayList<Edge>();
+		for(int i=0; i<solMaxSize; i++){
+			Edge a = edges[i];
+			for(int j=0; j<solMaxSize; j++){
+				Edge b = edges[j];
+				if(a != b && a.dest != b.origin && a.origin != b.dest){
+					if(edgeIntersect(a, b) && !conflictAlreadyFound(conflicts, a, b)){
+						conflicts.add(a);
+						conflicts.add(b);
+					}
+				}
+			}
+		}
+		return conflicts;
+	 }
 }
--- Trabalho-1/src/test1.java
+++ Trabalho-1/src/test1.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,67 +17,39 @@ public class test1 {
 			mem.add(x, y, i);
 		}
 	}
-	
-	public static Point subPoint(Point a, Point b) {
-		/**
-		 * Subtrai as coordenadas de dois pontos
-		 */
-		return new Point(a.x - b.x, a.y - b.y, -1);
+
+	public static void printConflicts(ArrayList<Edge> conflicts,Memory mem){
+		for(int i=0; i<conflicts.size(); i+=2){
+			Edge a,b;
+			a = conflicts.get(i);
+			b = conflicts.get(i+1);
+			Point start1, end1, start2, end2;
+
+			start1 = mem.points[a.origin];
+			end1 = mem.points[a.dest];
+
+			start2 = mem.points[b.origin];
+			end2 = mem.points[b.dest];
+
+			System.out.println(start1 + " " + end1 + " " + start2 + " " + end2);
+		}
+
 	}
-	
-	public static int crossProduct(Point a, Point b) {
-		/**
-		 * Produto vetorial de dois pontos
-		 */
-		return (a.x * b.y) - (a.y * b.x); 
-	}
-	
-	public static int halfStraddle(Point a, Point b, Point c) {
-		/**
-		 * Metade da operação de Straddle de dois pontos
-		 * (saber se um ponto interseta outro)
-		 */
-		return crossProduct(subPoint(c,a),subPoint(b,a));
-	}
-	
-	public static int straddleTest(Point a, Point b, Point c, Point d) {
-		/**
-		 * Retorna a posição relativa de um ponto a outro
-		 * (saber se um ponto interseta outro)
-		 */
-		return halfStraddle(c,d,a)*halfStraddle(c,d,b);
-	}
-	
-	public static boolean inBox(Point a, Point b, Point c) {
-		/**
-		 * Retorna se um ponto coincide noutro (?)
-		 */
-		return Math.min(a.x,b.x) <= c.x && c.x >= Math.max(a.x,b.x)
-				&& Math.min(a.y, b.y) <= c.y && c.y >= Math.max(a.y,b.y);
-	}
-	
-	public static boolean edgeIntersect(Memory mem, Edge i, Edge j) {
-		/**
-		 * Retorna se duas arestas se intersetam
-		 */
-		if(straddleTest(mem.points[i.origin], mem.points[i.dest], mem.points[j.origin], mem.points[j.dest])< 0
-				&& straddleTest(mem.points[j.origin], mem.points[j.dest], mem.points[i.origin], mem.points[i.dest])< 0)
-			return true;
-		else if(halfStraddle(mem.points[i.origin],mem.points[i.dest],mem.points[j.origin]) == 0)
-			if(inBox(mem.points[i.origin], mem.points[i.dest], mem.points[j.origin]))
-				return true;
-		else if(halfStraddle(mem.points[i.origin],mem.points[i.dest],mem.points[j.dest]) == 0)
-			if(inBox(mem.points[i.origin], mem.points[i.dest], mem.points[j.dest]))
-				return true;
-		else if(halfStraddle(mem.points[j.origin],mem.points[j.dest],mem.points[i.origin]) == 0)
-			if(inBox(mem.points[j.origin], mem.points[j.dest], mem.points[i.origin]))
-				return true;
-		else if(halfStraddle(mem.points[j.origin],mem.points[j.dest],mem.points[i.dest]) == 0)
-			if(inBox(mem.points[j.origin], mem.points[j.dest], mem.points[i.dest]))
-				return true;
-		return false;
+
+	public static void printEdges(Solution solution){
+		Memory mem = solution.mem;
+		
+		for(int i=0; i<solution.solMaxSize; i++){
+			Edge a = solution.edges[i];
+			Point start,end;
+			start = mem.points[a.origin];
+			end = mem.points[a.dest];
 			
+			System.out.println(start + " " + end);
+		}
 	}
+	
+	
 	
 	//nada de mais aqui, só serviu para testar o gerador de pontos e o greedy
 	//ainda nao testei a permutation nem nada
@@ -84,11 +57,39 @@ public class test1 {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int m = sc.nextInt();
+		//int m = sc.nextInt();
 		Memory mem = new Memory(n);
-		generatePoints(n, m, mem);
+
+		for(int i=0; i<n; i++){
+			int x,y;
+			x = sc.nextInt();
+			y = sc.nextInt();
+			mem.add(x,y,i);
+		}
+		Solution solution = new Solution(n,mem);
+
+		solution.permutation(mem);
+
+		ArrayList<Edge> conflicts = solution.getAllConflicts();
+
+		printEdges(solution);
+		System.out.println();
+
+		printConflicts(conflicts, mem);
+
+
 		
-		Solution sol = new Solution(n);
+
+
+		
+	
+
+	
+
+
+
+
+
 		
 		sc.close();
 	}
