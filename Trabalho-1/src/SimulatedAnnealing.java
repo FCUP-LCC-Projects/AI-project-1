ab5a0ea58be429c698d790781c7a8bb5b8b1074b
mbcsalmeida <61744828+mbcsalmeida@users.noreply.github.com>
2021-03-30 01:43:04 +0100

    Simulated Annealing a funcionar mas ainda a ser ajudastado

--- Trabalho-1/src/SimulatedAnnealing.java
+++ Trabalho-1/src/SimulatedAnnealing.java
@@ -2,51 +2,69 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class SimulatedAnnealing {
-	double T = 1; //temperature
-	final double Tmin = 0.0001; //temperature mínima limite
-	final double alpha = 0.9; //decremento 
-	final int numIterations = 100; //iterações até se diminuir T
+	double T; //temperature
+	int numIterations = 10; //iterações até se diminuir T
+	final double Tmin = 0.01; //temperature mínima limite
+	final double alpha = 0.95; //decremento 
 	
 	
 	public void simmulatedAnnealing(Solution s, Memory mem) {
 		Neighbours n = new Neighbours(mem);
-		int nConflicts = n.conflicts(s, true);
-		s.printSolution(mem);
-		System.out.println(n.edgeConflicts.size());
 		
 		Solution curSol = new Solution(s.solMaxSize, s.sol, s.edges);
+		int curConflicts = n.conflicts(curSol, true);
+		T = curConflicts;
 
 		Solution min = new Solution(s.solMaxSize);
 		int minConflict = Integer.MAX_VALUE;
 		
 		ArrayList<Solution> neighbours = n.resolveConflicts(curSol);
+		int count = 0;
 		
 		while(T > Tmin) {
-			for(int i=0; i<numIterations; i++) {
+			//System.out.println("T: "+T);
+			for(int i=0; i<numIterations && !neighbours.isEmpty(); i++) {
+				count++;
+				Solution best = getNewRandom(neighbours.size(), neighbours);
+				int bestConflicts = n.conflicts(best,false);
+				//System.out.println("best conflicts: "+bestConflicts+" curConflicts: "+curConflicts);
 				
-				if(nConflicts < minConflict) {
-					min = curSol;
-					minConflict = nConflicts;
-				}
-				
-				System.out.println("Neighbours");
-				System.out.println(neighbours.size());
-				n.printNeighbours(neighbours);
-				
-				Solution best = getNewRandom(neighbours.size(), neighbours); //não sabia que medida aplicar então por agora meti a random
-				int curConflicts = n.conflicts(best,false);
-				
-				double ap = Math.pow(Math.E, (nConflicts - curConflicts)/T);
-				
-				if(ap > Math.random()) {
+				if(bestConflicts < curConflicts) {
 					curSol = new Solution(best.solMaxSize, best.sol, best.edges);
-					nConflicts = n.conflicts(curSol, true);
+
+					curConflicts = n.conflicts(best, true);
+					
+					//System.out.println("second best conflicts: "+bestConflicts+" curConflicts: "+curConflicts);
+					
 					neighbours = n.resolveConflicts(curSol);
 				}
+				else {
+					double ap = Math.pow(Math.E, (double)(curConflicts - bestConflicts)/T);
+					//System.out.println("ap: "+ap);
+					
+				
+					if(ap > Math.random()) {
+						//System.out.println("ap better than random");
+						curSol = new Solution(best.solMaxSize, best.sol, best.edges);
+						
+						curConflicts = n.conflicts(best, true);	
+						
+						//System.out.println("second best conflicts: "+bestConflicts+" curConflicts: "+curConflicts);
+						
+						neighbours = n.resolveConflicts(curSol);
+					}
+				}
+				
+				if(curConflicts < minConflict) {
+					min = curSol;
+					minConflict = curConflicts;
+				}
 			}
+			if(minConflict == 0) break;
 			T *= alpha;
 		}
 		
+		System.out.println("iter: "+count);
 		System.out.println("conflicts: "+minConflict);
 		min.printSolution(mem);
 	}
