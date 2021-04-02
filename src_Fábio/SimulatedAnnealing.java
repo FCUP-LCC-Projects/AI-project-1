b6ad4ed7a3d0d3571e3768653f5072a3c77609b2
mbcsalmeida <61744828+mbcsalmeida@users.noreply.github.com>
2021-04-02 02:10:24 +0100

    Simulated Annealing a funcionar

--- src_Fábio/SimulatedAnnealing.java
+++ src_Fábio/SimulatedAnnealing.java
@@ -3,14 +3,12 @@ import java.util.Random;
 
 public class SimulatedAnnealing {
 	double T; //temperature
-	int numIterations = 10; //iterações até se diminuir T
+	int numIterations = 5; //iterações até se diminuir T
 	final double Tmin = 0.01; //temperature mínima limite
 	final double alpha = 0.95; //decremento 
 	
 	
-	public void simmulatedAnnealing(Solution s, Memory mem) {
-		Neighbours n = new Neighbours(mem);
-		
+	public void simmulatedAnnealing(Solution s, Memory mem) {		
 		Solution curSol = new Solution(s);
 		curSol.conflicts();
 		int curConflicts = curSol.totalConflicts;
@@ -33,34 +31,39 @@ public class SimulatedAnnealing {
 				int bestConflicts = best.totalConflicts;
 				//System.out.println("best conflicts: "+bestConflicts+" curConflicts: "+curConflicts);
 				
-				if(bestConflicts < curConflicts) {
+				double delta = bestConflicts - curConflicts;
+				if(delta < 0) {
 					curSol = new Solution(best);
 
-					curConflicts = best.totalConflicts;
+					curSol.conflicts();
+					curConflicts = curSol.totalConflicts;
 					
-					//System.out.println("second best conflicts: "+bestConflicts+" curConflicts: "+curConflicts);
 					
-					neighbours = n.resolveConflicts(curSol);
+					
+					curSol.resolveConflicts();
+					neighbours = curSol.neighbours;
 				}
 				else {
-					double ap = Math.pow(Math.E, (double)(curConflicts - bestConflicts)/T);
+					double ap = Math.pow(Math.E, (-1*delta)/T);
 					//System.out.println("ap: "+ap);
 					
 				
 					if(ap > Math.random()) {
-						//System.out.println("ap better than random");
+						System.out.println("ap better than random");
 						curSol = new Solution(best);
 						
-						curConflicts = best.totalConflicts;	
+						curSol.conflicts();
+						curConflicts = curSol.totalConflicts;
 						
-						//System.out.println("second best conflicts: "+bestConflicts+" curConflicts: "+curConflicts);
 						
-						neighbours = n.resolveConflicts(curSol);
+						
+						curSol.resolveConflicts();
+						neighbours = curSol.neighbours;
 					}
 				}
 				
 				if(curConflicts < minConflict) {
-					min = curSol;
+					min = new Solution(curSol);
 					minConflict = curConflicts;
 				}
 			}
@@ -71,6 +74,26 @@ public class SimulatedAnnealing {
 		System.out.println("iter: "+count);
 		System.out.println("conflicts: "+minConflict);
 		min.printSolution();
+	}
+	
+	public Solution lesserConflicts(ArrayList<Solution> neighbours) {
+		/**
+		 * Retorna o vizinho com menos conflitos e retorna o seu índice
+		 */
+		int best = 0;
+		int conflicts = Integer.MAX_VALUE;
+		
+		for(int i=0; i<neighbours.size(); i++) {
+			neighbours.get(i).conflicts();
+			neighbours.get(i).resolveConflicts();
+			int tmpConflicts = neighbours.get(i).totalConflicts;
+			
+			if(tmpConflicts < conflicts) {
+				best = i;
+				conflicts = tmpConflicts;
+			}
+		}
+		return neighbours.get(best);
 	}
 	
 	public Solution getNewRandom(int size, ArrayList<Solution> n) {
